08bbd65b82b689833c5c22b99fb23f1cd9b6293e
shin20090317 <shintaro060776@gmail.com>
2023-08-22 22:42:18 +0900

    2

--- security_group.tf
+++ security_group.tf
@@ -10,6 +10,14 @@ resource "aws_security_group" "allow" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    description = "All traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "allow all"
   }
