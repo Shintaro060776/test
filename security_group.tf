aa8e20f6ae3da3a7a63d89ab66814b21dfb83c8d
shin20090317 <shintaro060776@gmail.com>
2023-08-22 20:34:29 +0900

    2

---
+++ security_group.tf
@@ -0,0 +1,16 @@
+resource "aws_security_group" "allow" {
+    name = "allow"
+    description = "Allow all traffic"
+    vpc_id = aws_vpc.main.id
+
+    ingress {
+        description = "All traffic"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_block = ["0.0.0.0/0"]
+    }
+    tags = {
+        Name = "allow all"
+    }
+}
